927c806c60eef113aa7b6f57f233565cfec50cee
KACHIG <jose64rosado@gmail.com>
2024-09-20 12:51:11 -0400

    Made Slideshow
    added a slideshow to the site

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Code for mobile dropdown menu
 const menuBtn = document.querySelector('.menuHamburger')
    let menuOpen = false;
    menuBtn.addEventListener('click', () => {
@@ -13,3 +14,42 @@ const menuBtn = document.querySelector('.menuHamburger')
 menuBtn.addEventListener('click', () => {
     list.classList.toggle('show');
 });
+
+// Code for image slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  //makes slideshow automatic
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000);
+} 
+
